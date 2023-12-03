32b477d71ad11e14c7ee92d4cb0d8a34e830b075
Ruisu <luisxcam@gmail.com>
2023-12-03 17:49:30 -0500

    modified iteration to work on gears. added vertical scanning

--- Dec03/Dev03_p2.py
+++ Dec03/Dev03_p2.py
@@ -1,10 +1,10 @@
 from enum import Enum
-INVALID_CHAR = "."
+GEAR_CHAR = "*"
 
 
 class CharContextEnum(Enum):
     NUMERIC = 1
-    SYMBOL = 2
+    GEAR = 2
     INVALID = 3
 
 
@@ -18,71 +18,60 @@ def read_file(txtFile):
 def get_char_context(char):
     if (char.isdigit()):
         return CharContextEnum.NUMERIC
-    elif (char == INVALID_CHAR):
-        return CharContextEnum.INVALID
+    elif (char == GEAR_CHAR):
+        return CharContextEnum.GEAR
     else:
-        return CharContextEnum.SYMBOL
+        return CharContextEnum.INVALID
 
 
-def is_symbol_in_range(row, start_index, end_index):
-    if (row is None):
-        return False
-
+def scan_vertical_row(row, start_index, end_index):
+    amount = 0
+    locked = False
     for i in range(start_index, end_index + 1):
         char_context = get_char_context(row[i])
-        if (char_context == CharContextEnum.SYMBOL):
-            return True
-    return False
+        if (locked == False and char_context == CharContextEnum.NUMERIC):
+            amount += 1
+        locked = char_context == CharContextEnum.NUMERIC
+    return amount
 
 
-def get_is_valid_from_adjacent_row(prev_row, next_row, row_length, coordinates):
+def scan_around_symbol(prev_row, row, next_row, start_index, end_index):
+    total = 0
+    heat_map = {"above": 0, "bellow": 0, "next_to": 0}
+    if (prev_row is not None):
+        heat_map["above"] = scan_vertical_row(prev_row, start_index, end_index)
+        total += heat_map["above"]
+
+    if (next_row is not None):
+        heat_map["bellow"] = scan_vertical_row(
+            next_row, start_index, end_index)
+        total += heat_map["bellow"]
+
+
+def get_product_of_adjacent(prev_row, row, next_row, coord, row_length):
     max_index_value = row_length - 1
-    start_index = coordinates[0] - 1 if coordinates[0] > 0 else 0
-    end_index = coordinates[-1] + \
-        1 if coordinates[-1] < max_index_value else max_index_value
-
-    return is_symbol_in_range(prev_row, start_index, end_index) or is_symbol_in_range(next_row, start_index, end_index)
+    start_index = coord - 1 if coord > 0 else 0
+    end_index = coord + \
+        1 if coord < max_index_value else max_index_value
+    scan_around_symbol(prev_row, row, next_row, start_index, end_index)
 
 
 def get_values_from_row(prev_row, row, next_row):
     row_length = len(row)
     result = 0
-    is_valid_for_sum = False
-    prev_was_symbol = False
-    num_store = ""
-    coordinates = []
     for char_index in range(row_length):
-        is_last_iteration = char_index == row_length - 1
         char = row[char_index]
-
         char_context = get_char_context(char)
-        if (char_context == CharContextEnum.NUMERIC):
-            coordinates.append(char_index)
-            num_store += char
 
-        if (len(num_store) > 0 and (char_context == CharContextEnum.SYMBOL or prev_was_symbol)):
-            is_valid_for_sum = True
+        if (char_context != CharContextEnum.GEAR):
+            continue
 
-        if (not is_valid_for_sum and (char_context == CharContextEnum.INVALID or is_last_iteration) and len(num_store) > 0):
-            is_valid_for_sum = get_is_valid_from_adjacent_row(
-                prev_row, next_row, row_length, coordinates)
-
-        if ((char_context != CharContextEnum.NUMERIC or is_last_iteration) and len(num_store) > 0 and is_valid_for_sum):
-            result += int(num_store)
-            num_store = ""
-            coordinates = []
-            is_valid_for_sum = False
-
-        if (is_last_iteration or char_context == CharContextEnum.INVALID):
-            num_store = ""
-            coordinates = []
-            is_valid_for_sum = False
-
-        prev_was_symbol = char_context == CharContextEnum.SYMBOL
+        result += get_product_of_adjacent(prev_row,
+                                          row, next_row, char_index, row_length)
     return result
 
 
-def get_sum_of_symbol_to_number(text_arr):
+def get_product_of_gear(text_arr):
     result = 0
     prev_row = None
     for index in range(len(text_arr)):
@@ -97,4 +86,24 @@ def get_sum_of_symbol_to_number(text_arr):
 
 challenge_string = read_file("Dec03/Dec03.txt")  # 467835
 sample_string = read_file("Dec03/Dec03_Sample.txt")  # ?
-print(get_sum_of_symbol_to_number(challenge_string))
+print(get_product_of_gear(sample_string))
+
+
+# def is_symbol_in_range(row, start_index, end_index):
+#     if (row is None):
+#         return False
+
+#     for i in range(start_index, end_index + 1):
+#         char_context = get_char_context(row[i])
+#         if (char_context == CharContextEnum.GEAR):
+#             return True
+#     return False
+
+
+# def get_is_valid_from_adjacent_row(prev_row, next_row, row_length, coordinates):
+#     max_index_value = row_length - 1
+#     start_index = coordinates[0] - 1 if coordinates[0] > 0 else 0
+#     end_index = coordinates[-1] + \
+#         1 if coordinates[-1] < max_index_value else max_index_value
+
+#     return is_symbol_in_range(prev_row, start_index, end_index) or is_symbol_in_range(next_row, start_index, end_index)
